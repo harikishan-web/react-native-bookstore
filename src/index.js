f1009b60bbd9ab76579d0c1c51cb1818b1eb57c2
harikishan-web <harikishan3578@gmail.com>
2026-05-13 12:41:48 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
 const app = express();
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const PORT = process.env.PORT || 3001;
+
+job.start();
 
 app.use(express.json());
 app.use(cors());
