1557c6b75a9510145372f8c683d76da7fe283d40
harikishan-web <harikishan3578@gmail.com>
2026-05-17 23:04:25 +0530

    cron.js index.js

--- src/index.js
+++ src/index.js
@@ -15,13 +15,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Server is healthy",
-  });
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
