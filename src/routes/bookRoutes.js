ad4e5262f20b25ac36c4a25e67c089d37296aaa7
harikishan-web <harikishan3578@gmail.com>
2026-05-18 15:39:57 +0530

    bookRoutes.js added

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -158,6 +158,9 @@ const router = express.Router();
 
 router.post("/", protectRoute, async (req, res) => {
   try {
+    console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+    console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+    console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
     const { title, caption, rating, image } = req.body;
 
     if (!image || !title || !caption || !rating) {
